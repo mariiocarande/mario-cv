5e654a3a7845a66915aff40f46bc65c59c516fdb
mariiocarande <mdc.mariio@gmail.com>
2021-10-30 02:42:06 -0300

    UPDATE: add description

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaTwitter, FaTwitch, FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import { Button, Flex, HStack, Image, useColorModeValue, Link } from '@chakra-ui/react';
+import { Button, Box, Flex, Heading, HStack, Image, useColorModeValue, Link } from '@chakra-ui/react';
 
 const Home: React.FC = () => {
     const colorMode = useColorModeValue("gray.300", "gray.700");
@@ -8,6 +8,9 @@ const Home: React.FC = () => {
     return (
       <Flex>
           <Flex alignItems="center" background={colorMode} direction="column" p={12} rounded={6}>
+            <Heading as="h2" mb="5" isTruncated size="3xl">
+              Hi welcome! 
+            </Heading>
             <Image
               alt="Mario Carande Scarafía"
               borderRadius="full"
@@ -15,7 +18,11 @@ const Home: React.FC = () => {
               marginBottom="30px"
               src="https://bit.ly/3bv0Dwx"
             />
-
+            <Box h="180px" w="450px">
+              {`I'm Mario Carande Scarafía I'm a front-end developer actually working with ReactJS.\n
+                I like learning about new technologies and working with teams that know how to follow good practices.\n 
+                I've been working with Nimble.la for 2 years.`}
+            </Box>
             <HStack>
               <Button colorScheme="linkedin" leftIcon={<FaLinkedinIn />}>
                 <Link isExternal href="https://bit.ly/3GxMld5">
