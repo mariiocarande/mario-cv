1cad126429e20d0b520ea20ec9f85661018d82a4
mariiocarande <mdc.mariio@gmail.com>
2022-06-12 21:33:22 -0300

    UPDATE: remove twitch link and add spinner

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,5 +7,6 @@ import { App } from "./App"
 test("renders learn react link", () => {
   render(<App />)
   const linkElement = screen.getByText(/learn chakra/i)
+
   expect(linkElement).toBeInTheDocument()
 })
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,51 +1,68 @@
-import React, { ReactElement } from 'react';
-import { FaTwitter, FaTwitch, FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import { Button, Box, Flex, Heading, HStack, Image, useColorModeValue, Link } from '@chakra-ui/react';
+import React, { ReactElement, useState, useEffect } from 'react';
+import { FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { Button, Box, Flex, Heading, HStack, Image, useColorModeValue, Link, Spinner } from '@chakra-ui/react';
 
 import profilePicture from '../assets/img/profile-picture.jpg';
 
 const Home: React.FC = () => {
-    const colorMode = useColorModeValue("gray.300", "gray.700");
-    const renderButton = (color: string, icon: ReactElement, socialMediaText: string, url: string) => (
-      <Link isExternal href={url} style={{textDecoration : 'none'}}>
-        <Button colorScheme={color} leftIcon={icon}>
-          {socialMediaText}
-        </Button>
-      </Link>
-    );
+  const [loader, setLoader] = useState<boolean>(true);
+
+  const colorMode = useColorModeValue("gray.300", "gray.700");
+  const renderButton = (color: string, icon: ReactElement, socialMediaText: string, url: string) => (
+    <Link isExternal href={url} style={{textDecoration : 'none'}}>
+      <Button colorScheme={color} leftIcon={icon}>
+        {socialMediaText}
+      </Button>
+    </Link>
+  );
+  
+  useEffect(() => {
+    setTimeout(() => setLoader(false), 1000);
+  }, []);
+
+  const displayBody = () => {
+    if (loader) return <Spinner size="lg" />;
 
     return (
-      <Flex>
-          <Flex alignItems="center" background={colorMode} direction="column" p={12} rounded={6}>
-            <Heading 
-              isTruncated 
-              as="h2"
-              mb="5" 
-              size="3xl"
-            >
-              Hi welcome! 
-            </Heading>
-            <Image
-              alt="profilePicture"
-              borderRadius="full"
-              boxSize="150px"
-              marginBottom="30px"
-              src={profilePicture}
-            />
-            <Box h="180px" w="450px">
-              {`I'm Mario Carande Scarafía I'm a front-end developer actually working with ReactJS.\n
-                I like learning about new technologies and working with teams that know how to follow good practices.\n 
-                I've been working with Nimble.la for 2 years.`}
-            </Box>
-            <HStack>
-              {renderButton("linkedin",<FaLinkedinIn />,"LinkedIn","https://bit.ly/3GxMld5")}
-              {renderButton("messenger",<FaGithub />,"Github","https://bit.ly/3w5aFhx")}
-              {renderButton("twitter",<FaTwitter />,"Twitter","https://bit.ly/3bmBwMD")}
-              {renderButton("purple",<FaTwitch />,"Twitch","https://bit.ly/3GCFkYe")}
-            </HStack>
-          </Flex>
-      </Flex>
+      <>
+        <Heading
+          isTruncated
+          as="h2"
+          mb="5"
+          size="3xl"
+        >
+          Hi welcome!
+        </Heading>
+      
+        <Image
+          alt="profilePicture"
+          borderRadius="full"
+          boxSize="150px"
+          loading="lazy"
+          marginBottom="30px"
+          src={profilePicture}
+        />
+        <Box h="180px" w="450px">
+          {`I'm Mario Carande Scarafía I'm a front-end developer actually working with ReactJS.\n
+            I like learning about new technologies and working with teams that know how to follow good practices.\n 
+            I've been working with Nimble.la for 2 years.`}
+        </Box>
+        <HStack>
+          {renderButton("linkedin", <FaLinkedinIn />, "LinkedIn", "https://bit.ly/3GxMld5")}
+          {renderButton("messenger", <FaGithub />, "Github", "https://bit.ly/3w5aFhx")}
+          {renderButton("twitter", <FaTwitter />, "Twitter", "https://bit.ly/3bmBwMD")}
+        </HStack>
+      </>
     );
+  };
+
+  return (
+    <Flex>
+      <Flex alignItems="center" background={colorMode} direction="column" p={12} rounded={6}>
+        {displayBody()}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
