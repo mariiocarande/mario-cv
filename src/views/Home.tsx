76e81c2cda9c0ef5fb45eb4c737f9490528231b0
Mario Carande Scarafia <mdc.mariio@gmail.com>
2023-06-21 10:21:07 -0300

    Modify home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,21 +1,36 @@
-import React, { ReactElement, useState, useEffect } from 'react';
-import { FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import { Button, Box, Flex, Heading, HStack, Image, useColorModeValue, Link, Spinner } from '@chakra-ui/react';
+import React, { ReactElement, useState, useEffect } from "react";
+import { FaTwitter, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import {
+  Button,
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  useColorModeValue,
+  Link,
+  Spinner,
+} from "@chakra-ui/react";
 
-import profilePicture from '../assets/img/profile-picture.jpg';
+import profilePicture from "../assets/img/profile-picture.jpg";
 
 const Home: React.FC = () => {
   const [loader, setLoader] = useState<boolean>(true);
 
   const colorMode = useColorModeValue("gray.300", "gray.700");
-  const renderButton = (color: string, icon: ReactElement, socialMediaText: string, url: string) => (
-    <Link isExternal href={url} style={{textDecoration : 'none'}}>
+  const renderButton = (
+    color: string,
+    icon: ReactElement,
+    socialMediaText: string,
+    url: string
+  ) => (
+    <Link isExternal href={url} style={{ textDecoration: "none" }}>
       <Button colorScheme={color} leftIcon={icon}>
         {socialMediaText}
       </Button>
     </Link>
   );
-  
+
   useEffect(() => {
     setTimeout(() => setLoader(false), 1000);
   }, []);
@@ -25,15 +40,10 @@ const Home: React.FC = () => {
 
     return (
       <>
-        <Heading
-          isTruncated
-          as="h2"
-          mb="5"
-          size="3xl"
-        >
+        <Heading isTruncated as="h2" mb="5" size="3xl">
           Hi welcome!
         </Heading>
-      
+
         <Image
           alt="profilePicture"
           borderRadius="full"
@@ -45,12 +55,28 @@ const Home: React.FC = () => {
         <Box h="180px" w="450px">
           {`I'm Mario Carande Scarafía I'm a front-end developer actually working with ReactJS.\n
             I like learning about new technologies and working with teams that know how to follow good practices.\n 
-            I've been working with Nimble.la for 2 years.`}
+            I worked on NimbleLA for 4 years.
+            I'm now looking for new challengues.`}
         </Box>
         <HStack>
-          {renderButton("linkedin", <FaLinkedinIn />, "LinkedIn", "https://bit.ly/3GxMld5")}
-          {renderButton("messenger", <FaGithub />, "Github", "https://bit.ly/3w5aFhx")}
-          {renderButton("twitter", <FaTwitter />, "Twitter", "https://bit.ly/3bmBwMD")}
+          {renderButton(
+            "linkedin",
+            <FaLinkedinIn />,
+            "LinkedIn",
+            "https://bit.ly/3GxMld5"
+          )}
+          {renderButton(
+            "messenger",
+            <FaGithub />,
+            "Github",
+            "https://bit.ly/3w5aFhx"
+          )}
+          {renderButton(
+            "twitter",
+            <FaTwitter />,
+            "Twitter",
+            "https://bit.ly/3bmBwMD"
+          )}
         </HStack>
       </>
     );
@@ -58,7 +84,13 @@ const Home: React.FC = () => {
 
   return (
     <Flex>
-      <Flex alignItems="center" background={colorMode} direction="column" p={12} rounded={6}>
+      <Flex
+        alignItems="center"
+        background={colorMode}
+        direction="column"
+        p={12}
+        rounded={6}
+      >
         {displayBody()}
       </Flex>
     </Flex>
